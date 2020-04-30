b54b2b46d7708d1173c73316fbebb97a61477431
patrick <admin@irondigital.com>
2020-04-30 00:12:42 -0400

    created a more dynamic implementation

--- BreadthFIrstSearch.cpp
+++ BreadthFIrstSearch.cpp
@@ -9,12 +9,13 @@ bool no_nodes(std::queue<int> *not_seen)
 	if (!not_seen->empty()) return true;
 	return false;
 }
-std::vector<int> find_all_nodes(std::vector<std::vector<int>>*graph, std::map<int, bool>* seen,int current_node)
+template<typename T>
+std::vector<T> find_all_nodes(std::vector<std::vector<T>>*graph, std::map<T, bool>* seen,T current_node)
 {
 
 	std::vector<int> adjacent;
-	if ((*seen)[current_node])
-		return adjacent;
+	if ((*seen)[current_node]) return adjacent;
+		
 	for (int i = 0; i < graph->at(current_node).size();i++)
 	{
 		if(graph->at(current_node).at(i) !=0 && !(*seen)[current_node])
@@ -24,39 +25,27 @@ std::vector<int> find_all_nodes(std::vector<std::vector<int>>*graph, std::map<in
 	(*seen)[current_node] = true;
 	return adjacent;
 }
-
-void build_graph(std::vector<std::vector<int>>* graph, std::map<int,bool> *seen,int i,int j)
+template<typename T>
+void build_graph(std::vector<std::vector<T>>* graph, std::map<T,bool> *seen,T i,T j)
 {
 	graph->at(i).at(j) = 1;
 	(*seen)[i] = false;
 }
-
 int main()
 {
-	int graph[8][8] = { 0 };
+	//int graph[8][8] = { 0 };
 	std::vector<std::vector<int>> graph1(8, std::vector<int>(8, 0));
 	
 	std::queue<int> not_seen;
 	std::map<int,bool> seen;
-	
+
 	not_seen.push(0);
+	std::vector<int> edges = { 0,1,0,4,1,5,1,4,2,4,2,1,3,5,3,7,4,6,5,7};
+	for(auto i {0};i + 2 < edges.size();i += 2)
+	{
+		build_graph(&graph1, &seen, edges[i ],edges[i + 2]);
+	}
 	
-	build_graph(&graph1, &seen, 3, 1);
-	build_graph(&graph1, &seen, 0, 0);
-	build_graph(&graph1, &seen, 0, 3);
-	build_graph(&graph1, &seen, 1, 2);
-	build_graph(&graph1, &seen, 1, 0);
-	build_graph(&graph1, &seen, 1, 0);
-	build_graph(&graph1, &seen, 2, 3);
-	build_graph(&graph1, &seen, 2, 1);
-	build_graph(&graph1, &seen, 5, 1);
-	build_graph(&graph1, &seen, 4, 1);
-	build_graph(&graph1, &seen, 4, 2);
-	build_graph(&graph1, &seen, 5, 4);
-	build_graph(&graph1, &seen, 6, 2);
-	build_graph(&graph1, &seen, 6, 5);
-	build_graph(&graph1, &seen, 3, 6);
-	build_graph(&graph1, &seen, 7, 7);
 	while(no_nodes(&not_seen))
 	{
 		for (auto i : find_all_nodes(&graph1, &seen, not_seen.front()))
